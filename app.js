f508875b1bb6bf8181dee3b3a0ce372f49a190d5
andreslage <andreslage97@gmail.com>
2024-07-04 11:25:22 +0200

    más explicaciones

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ var i;
 for (i = 0; i < close.length; i++) {
   close[i].onclick = function () {
     //coge el 'li' y lo saca, pero no lo elimina
-    var div = this.parentElement;
-    div.style.display = "none";
+    var linea = this.parentElement;
+    linea.style.display = "none";
   };
 }
 
@@ -44,7 +44,7 @@ function newElement() {
     document.getElementById("myUL").appendChild(li);
   }
   document.getElementById("myInput").value = "";
-
+  //le crea una X a estas también
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
   span.className = "close";
